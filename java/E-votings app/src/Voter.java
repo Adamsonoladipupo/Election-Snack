fb976369d8ddcab701de3ef464bb2eadd538b9d0
OlaAdams <adamsonoladipupo@gmail.com>
2025-12-24 04:20:25 +0100

    new update

---
+++ java/E-votings app/src/Voter.java
@@ -0,0 +1,37 @@
+public class Voter {
+    private String name;
+    private String email;
+    private String voterID;
+
+    public Voter(){
+
+    }
+    public Voter(String name, String email){
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getVoterID() {
+        return voterID;
+    }
+
+    public void setVoterID(String voterID) {
+        this.voterID = voterID;
+    }
+}
